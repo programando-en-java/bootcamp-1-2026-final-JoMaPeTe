531a4c38c793239eeae67025492d31ef0af5334d
JoMaPeTe <jmperezterrero@gmail.com>
2026-05-01 18:52:00 +0200

    chore: configuracion inicial de modulos y orquestacion con docker

---
+++ airline-system/checkin-service/src/main/java/com/programandoenjava/chekinservice/ChekinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.programandoenjava.chekinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChekinServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChekinServiceApplication.class, args);
+    }
+
+}
---
+++ airline-system/checkin-service/src/test/java/com/programandoenjava/chekinservice/ChekinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programandoenjava.chekinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChekinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
